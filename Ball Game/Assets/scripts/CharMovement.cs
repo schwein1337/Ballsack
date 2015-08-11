6e4aae70bd5f11811e2508bc96703a16ec72c7e7
Mathias Gotha Vest <mathias@r2n.dk>
2015-08-11 22:43:28 +0200

    Inital commit of the unity files

---
+++ Ball Game/Assets/scripts/CharMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharMovement : MonoBehaviour
+{
+	int maxSpeed = 5;
+	public float speed = 2.0f;
+	public bool grounded = false;
+
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+
+
+
+		if(grounded)
+		{
+			GetComponent<Rigidbody2D>().AddForce(speed*movement);
+		}
+
+		if(GetComponent<Rigidbody2D>().velocity.magnitude >= maxSpeed)
+		{
+		
+			speed = maxSpeed;
+		}
+		print("Grounded: "+grounded);
+		print ("velocity: "+GetComponent<Rigidbody2D>().velocity.x+","+GetComponent<Rigidbody2D>().velocity.y);
+		print ("Speed: "+GetComponent<Rigidbody2D>().velocity.magnitude);
+
+
+
+
+	}
+
+
+
+	void onCollisionEnter()
+	{
+		grounded = true;
+	}
+
+	void onCollisionExit()
+	{
+		grounded = false;
+	}
+
+}
